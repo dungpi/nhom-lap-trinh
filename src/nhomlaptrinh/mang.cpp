a8c1b83a649817e9363039b17921ed393b6f7f9b
Dungpi <phamdung.080602@gmail.com>
2020-12-31 17:47:24 +0700

    # xóa hàm xuất

--- src/nhomlaptrinh/mang.cpp
+++ src/nhomlaptrinh/mang.cpp
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <math.h>
 #define MAX 50
 void nhapMang(int a[], int &n);
 void xuatmang(int a[],int n);
 int kiemtraToanChan(int a[], int n);
-void xuatMangChan (int a[],int n);
+int kiemtraCoTonTaiSoChan(int a[], int n);
+void xuatTontaiSoChan(int a[], int n);
+int sosanhChanle(int a[], int n);
+void xuatsosanhChanLe(int a[], int n);
+int tinhTileChanle(int a[], int n);
+void xuatTileChanLe(int a[], int n);
 int main(){
     int a[MAX],n;
         printf ("Nhap so phan tu n: ");
@@ -15,7 +21,7 @@ int main(){
         }
     }while (n <= 0);
     nhapMang (a,n);
-    printf ("Cac phan tu:");
+    printf ("\nCac phan tu:   ");
     xuatmang (a,n);
     	if (kiemtraToanChan(a,n) == -1){
 	    	printf("\nMang khong toan so chan!");
@@ -23,7 +29,7 @@ int main(){
 		else{
 			printf("\nMang toan so chan!");
 		}
-	xuatMangChan (a,n);
+	   printf("\nSo sanh so luong phan tu chan va le: %d ",sosanhChanLe(a,n));	
     return 0; 
 }
 void nhapMang(int a[], int &n){
@@ -47,16 +53,42 @@ int kiemtraToanChan(int a[], int n){
 			 break;
 		}
 			return 1;	 
+}
+int kiemtraCoTonTaiSoChan(int a[], int n){
+	for(int i=0; i<n; i++){
+		if(a[i]%2==0){
+			printf("\nMang co ton tai so chan!");
+			return 1;
+			break;
+		}
+		return 1;
     }
-void xuatMangChan (int a[],int n){
-	int temp;
-	printf ("\nCac so chan :");
-	for (int i = 0; i < n; i++){
-		for (int j = 0; j < n;j++){
-			if (a[i]%2 == 0){
-			a[i] = a[j]; 
-					printf ("\t%d",a[j]);
-			}
+}
+void xuatTontaiSoChan(int a[], int n){
+	for(int i=0; i<n; i++){
+		printf("%d",a[i]);
+	}
+}
+int sosanhChanLe(int a[], int n){
+	int demChan = 0;
+	for(int i=0; i<n; i++){
+		if(a[i]%2==0){
+			demChan++;
 		}
 	}
+	    if(demChan > n - demChan){
+	    	return -1;
+		}
+		else (demChan < n - demchan){
+			 return 1;
+		}
+		     return 0;
+}
+void xuatsosanhChanLe(int a[], int n){
+	for(int i=0; i<n; i++){
+		printf("%d",a[i]);
+	}
+}
+int tinhTileChanle(int a[], int n){
+	
 }
